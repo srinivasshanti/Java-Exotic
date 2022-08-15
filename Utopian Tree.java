018caca311fcea1f8005fd29bdb24af64fe8ec7f
Srinivas Shanti <71806440+srinivasshanti@users.noreply.github.com>
2022-08-15 17:22:46 +0530

    Update Utopian Tree.java

--- Utopian Tree.java
+++ Utopian Tree.java
@@ -4,12 +4,15 @@ import java.util.Vector;
 public class Solution {
     public static void main(String[] args) {
         Scanner stdin = new Scanner(System.in);
-        int numCases = stdin.nextInt(); 
-      
+        int numCases = stdin.nextInt(); //number of cases
+      //The Vector class implements a growable array of objects
         Vector<Long> cache = new Vector<>();
-        cache.add(1L);
+        cache.add(1L);  //add one to the array
+        
+        
         for (int i = 0; i < numCases; i++) {
-            int numCycles = stdin.nextInt();
+            int numCycles = stdin.nextInt(); //number of cycles
+            
             while(cache.size() <= numCycles) {
                 if (cache.size() % 2 == 0) {
                     cache.add(cache.lastElement() + 1);                
